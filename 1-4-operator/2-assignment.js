9959fb48328229123a7b67e317738ca658091c94
hsp64 <pphs604@gmail.com>
2025-05-27 12:38:40 +0900

    string,other-type type-casting

---
+++ 1-4-operator/2-assignment.js
@@ -0,0 +1,15 @@
+
+let number = 5;
+number += 3; //number = number + 3;
+number -= 6;
+number *= 5;
+number /= 2;
+number **= 4;
+number %= 10;
+
+
+console.log(`number: ${number}`);
+
+let num = 3;
+num++; // 4
+// num += 1; // 4
