6f9910683a3a917b50f6d00cfdaf0d40795c3c63
hsp64 <pphs604@gmail.com>
2025-05-30 09:33:20 +0900

    예제 2개 붚ㄹ이

--- 1-6-loop/practice3.js
+++ 1-6-loop/practice3.js
@@ -7,3 +7,19 @@ for (let i = 2; i < numIndex; i *= 2) {
     // i;
 }
 alert(`${result}`);
+
+//특정 양의 정수를 입려받고 1부터 해당 입력숫자까지의 범위의
+//2의 제곱수를 가로로 출력
+let number  = +prompt('양의 정수를 입력하세요.');
+
+if (isNaN(number)) {
+    alert('숫자만 입력하세요!');
+} else if (number <= 0) {
+    alert('1 이상의 양수만 입력하세요!');
+}
+let resultText = '';
+for (let i = 2; i <= number; i *= 2) {
+    resultText += `${i} `;
+}
+
+alert(resultText);
