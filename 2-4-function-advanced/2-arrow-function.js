55fbcb41414dcca9865bd281dd7c960f162b070b
hsp64 <pphs604@gmail.com>
2025-06-05 11:53:25 +0900

    arrow-function, call-back

---
+++ 2-4-function-advanced/2-arrow-function.js
@@ -0,0 +1,27 @@
+
+// 함수 선언문 방식
+// function add(n1, n2) {
+//     return n1 + n2;
+// }
+
+// 함수 표현식 방식
+// const add = function () {
+//     return n1 + n2;
+// };
+
+
+// 화살표 함수 방식 (ES6)
+const add = (n1, n2) => n1 + n2; // n1, n2 를 받아서 + 한 값을 리턴
+
+const r1 = add(10,20);
+console.log(`r1: ${r1}`);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(25));
+
+const sayHello = (nickName) => console.log(`${nickName}님 안녕~~`);
+
+sayHello('크룽이');
+
+const pow = n => n ** 2;
+
