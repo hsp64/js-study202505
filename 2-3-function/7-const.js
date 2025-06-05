a8c86cf6a74d2a0b3c9933f81ee6e2101e03e299
hsp64 <pphs604@gmail.com>
2025-06-05 10:53:40 +0900

    const습관화 , function-expression

---
+++ 2-3-function/7-const.js
@@ -0,0 +1,27 @@
+
+const x = 10;
+// x = 20;
+console.log(`x: ${x}`);
+
+let numbers = [10, 20, 30, 40];
+// let copyNumbers = numbers.slice();
+let copyNumbers = [...numbers];
+
+
+/*
+for (const n of numbers) {
+    copyNumbers.push(n);
+}
+*/
+
+numbers[1] = 99;
+copyNumbers[2] = 777;
+
+console.log('numbers:',numbers);
+console.log('numbers:', copyNumbers);
+
+
+const dog = {
+    name: '뽀삐'
+};
+
